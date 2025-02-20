32e82fcd907bec8c2507879ea82b025d61197703
ubgultra <155318917+ubgultra@users.noreply.github.com>
2025-02-20 18:05:10 -0500

    Update ServiceWorker.js

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,10 +1,10 @@
 const cacheName = "reun-Block Blast Puzzle-1.1";
 const contentToCache = [
-    "Build/BlockBlast1.1-8.loader.js",
-    "Build/BlockBlast1.1-8.framework.js.unityweb",
-    "Build/BlockBlast1.1-8.data.unityweb",
-    "Build/BlockBlast1.1-8.wasm.unityweb",
-    "TemplateData/style.css"
+    "https://cdn.jsdelivr.net/gh/ubgultra/Block-Blast@main/Build/BlockBlast1.1-8.loader.js",
+    "https://cdn.jsdelivr.net/gh/ubgultra/Block-Blast@main/Build/BlockBlast1.1-8.framework.js.unityweb",
+    "https://cdn.jsdelivr.net/gh/ubgultra/Block-Blast@main/Build/BlockBlast1.1-8.data.unityweb",
+    "https://cdn.jsdelivr.net/gh/ubgultra/Block-Blast@main/Build/BlockBlast1.1-8.wasm.unityweb",
+    "https://cdn.jsdelivr.net/gh/ubgultra/Block-Blast@main/TemplateData/style.css"
 
 ];
 
